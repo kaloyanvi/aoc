e5384773f86d963ee4398b38fd2f3825c808b591
kvidelov <kvidelov@protonmail.com>
2020-12-07 14:40:39 +0100

    Solutions Day 7.

---
+++ Python/day_7/part_two.py
@@ -0,0 +1,51 @@
+
+# used to count the bags
+def findBag(starting_bag, within_bag, possible_bags):
+    for container, contained_bags in within_bag.items():
+        
+        for contained in contained_bags:  # iterating over the bags contained within the main bag
+
+            # check for bag - first is shiny gold
+            if starting_bag == contained["type"]:
+
+                if container not in possible_bags:  # check if its outer bag
+                    possible_bags.append(container)  #  add the container bag
+
+                # recursion baby
+                possible_bags = findBag(container, within_bag, possible_bags) 
+
+    return possible_bags
+
+# used to store all bag data
+container_bag_data = {}  # dict for all bags
+with open('Python/day_7/input.txt') as f:
+
+    lines = f.readlines()
+    for line in lines:
+        line_format = line.strip()  # remove whitespaces 
+        
+        contained_within = line_format.split('bags contain')  # list of elems. ; [0] - contains; [1] - contained
+        container_bag = contained_within[0].strip()  
+        contained_bags = contained_within[1].strip().split(',') 
+
+        container_bag_data[container_bag] = []  # will be filled with all contained bags of a container_bag bag
+
+        if contained_within[1] != " no other bags.":  # bags that can contain other bags
+            
+            for bag in contained_bags:
+                contained_bag = {}  # dict for local bag
+                contained_bag["count"] = int(bag.strip()[0])
+                contained_bag["type"] = bag.strip().rstrip('.')[1:-4].strip()
+                container_bag_data[container_bag].append(contained_bag)  # adding the bag dict to the dict with all bags
+                
+        else:  # bags that cant contain other bags
+            pass
+
+def findBag2(starting_bag, container_big_data):
+    count = 0
+    for bag in container_big_data[starting_bag]:  # iterate over the values of the input bag
+        count += bag["count"] + bag["count"] * findBag2(bag["type"], container_big_data)  # recursively go deeper
+
+    return count
+
+print("Answer %i"%findBag2("shiny gold", container_bag_data))
