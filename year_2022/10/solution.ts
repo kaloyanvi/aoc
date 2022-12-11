eb01fc03b39a8ef3a71d2df2e450f979f3d2bc9a
Kaloyan Videlov <kvidelov@protonmail.com>
2022-12-12 00:24:28 +0100

    day 10 part 2 and cleanup

--- year_2022/10/solution.ts
+++ year_2022/10/solution.ts
@@ -1,59 +1,49 @@
 import { loadFile } from '../../loadFile'
 
 const input = loadFile('/year_2022/10/input.txt')
-// addX V takes two cycles to complete and adds V to X
-// noop takes one cycle to complete, does nothing
-
-const lastCycle = 220
 
 type Modifier = number | null
 
-const cpuCycle = (cmdIndex, currentCycle, commands, X, mod: Modifier, recs: number[]) => {
+const image = [[]]
+const cpuCycle = (cmdIndex, currentCycle, commands, X, mod: Modifier, recs: number[], currentRow=0) => {
   const measureCycles = [20, 60, 100, 140, 180, 220]
   if (cmdIndex >= commands.length) {
-    console.log('\tdone on cycle ', currentCycle)
     const sum = recs.reduce((a, b) => a + b, 0)
-    console.log('sum of all recordings: ', sum)
-    return X
+    image.forEach((row) => {
+      console.log(row.join(''))
+    })
+    return sum
   }
   
   const signalStrength = X * currentCycle
   const [currentCmd, modifier] = commands[cmdIndex].split(' ')
-  console.log('on cycle', currentCycle)
-  // console.log('CPU state: ', cpuState)
-  console.log('X: ', X)
-  console.log('signal strength: ', signalStrength)
-  console.log('\tcurrent cmd', currentCmd)
   
-  if (measureCycles.includes(currentCycle)) {
-    console.log('\tmeasure signal strength')
-    console.log(`cycle (${currentCycle}) * X (${X}) = ${signalStrength}`, signalStrength)
-    recs.push(signalStrength)
+  if (measureCycles.includes(currentCycle)) recs.push(X * currentCycle)
+  
+  let nextRow = currentRow
+  if (currentCycle % 40 === 0) {
+    nextRow = currentRow + 1
+    image.push([])
   }
-  if (mod) {
-    console.log('\tcpu is busy')
-    const modifiedX = X + mod
 
-    return cpuCycle(cmdIndex, currentCycle + 1, commands, modifiedX, null, recs)
+  const coverage = [X - 1, X, X + 1]
+  if (coverage.includes(image[currentRow].length)) image[currentRow].push('#')
+  else image[currentRow].push('.')
+
+  if (mod) {
+    const modifiedX = X + mod
+    return cpuCycle(cmdIndex, currentCycle + 1, commands, modifiedX, null, recs, nextRow)
   }
 
   if (currentCmd === 'noop') {
-    console.log('\t do nothing')
-    return cpuCycle(cmdIndex + 1, currentCycle + 1, commands, X, null, recs)
+    return cpuCycle(cmdIndex + 1, currentCycle + 1, commands, X, null, recs, nextRow)
   }
-
-  // console.log('\tmodifier', parseInt(modifier))
-  // const modifiedX = X + parseInt(modifier)
-  return cpuCycle(cmdIndex + 1, currentCycle + 1, commands, X, parseInt(modifier), recs)
-  
+  return cpuCycle(cmdIndex + 1, currentCycle + 1, commands, X, parseInt(modifier), recs, nextRow)
 }
 
-
-
 const solution = (input) => {
-  const X = cpuCycle(0, 1, input, 1, null, [])
-  console.log('\n\nX after last cycle: ', X)
-
+  const sum = cpuCycle(0, 1, input, 1, null, [], 0)
+  console.log('sum of all recordings: ', sum)
 }
 
 solution(input.split('\n'))
