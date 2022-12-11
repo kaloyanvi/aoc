8e9ad1fbbc1dfb2d532c7875b31c5651ff2a52c1
Kaloyan Videlov <kvidelov@protonmail.com>
2022-12-11 14:46:12 +0100

    Day 9 part 2

--- year_2022/9/solution.ts
+++ year_2022/9/solution.ts
@@ -41,19 +41,41 @@ const moveTail = (tail, head) => {
   return tail
 }
 
+const createNewTail = (prevTail, allTails) => {
+  const prevTailOnStartPos = prevTail.x === 0 && prevTail.y === 0
+  const hasAllTails = allTails.length === 9
+  if (prevTailOnStartPos || hasAllTails) return allTails
+  allTails.push({ x: 0, y: 0 })
+  return allTails
+}
+
+const endOfTailVisitedPos = []
+const moveTails = (allTails, head) => {
+  allTails.forEach((tail, index) => {
+    if (index === 0) tail = moveTail(tail, head)
+    else tail = moveTail(tail, allTails[index - 1])
+    if (index === allTails.length - 1) endOfTailVisitedPos.push(`${tail.x}:${tail.y}`)
+  })
+  const newTails = createNewTail(allTails[allTails.length - 1], allTails)
+  return newTails
+}
+
 const solution = (lines: string[]) => {
-  const tailVisitedPositions = [] 
+  const tailVisitedPositions = []
   let head = { x: 0, y: 0 }
   let tail = { x: 0, y: 0 }
+  let allTails = [tail]
   lines.forEach((line) => {
     const [direction, distance] = line.split(' ')
     for (let i = 0; i < parseInt(distance); i++) {
       head = moveHead(head, direction)
       tail = moveTail(tail, head)
-      tailVisitedPositions.push(`${tail.x}-${tail.y}`)
+      allTails = moveTails(allTails, head)
+      tailVisitedPositions.push(`${tail.x}:${tail.y}`)
     }
   })
   console.log('# visited postions: ', [...new Set(tailVisitedPositions)].length)
+  console.log('# visited postions long: ', [...new Set(endOfTailVisitedPos)].length - 1)
 }
 
 solution(input.split('\n'))
